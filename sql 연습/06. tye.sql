4fade49a95ce1045fa79c8016150cc508ff51283
bit <bit@DESKTOP-HPBJEHN>
2025-08-14 17:42:32 +0900

    sql 06 tye

--- sql 연습/06. tye.sql
+++ sql 연습/06. tye.sql
@@ -2,8 +2,10 @@
 select '12345' + 10, cast('12345' as signed) + 10
 	from dual;
     
+    
 select date_format(cast('2025-08-14' as date), '%Y년 %m월 %d일')
 	from dual;
+    
     
 select cast(cast(1-2 as unsigned) as signed),
 	   cast(cast(1-2 as unsigned) as int),
